b78cc9b993abd94adc5ba4f8a7f9c29e7cee5a82
mutantmonkey <mutantmonkey@mutantmonkey.in>
2023-12-07 02:05:47 -0800

    Update boot.cmd for ODROID-C2

--- boot.cmd
+++ boot.cmd
@@ -1,10 +1,10 @@
-fatload mmc 2:1 0x40008000 vmlinuz
+fatload mmc 2:1 0x11000000 vmlinuz
 
-fatload mmc 2:1 0x42000000 cmdline.txt
-setexpr.s bootargs *0x42000000
+fatload mmc 2:1 0x12000000 cmdline.txt
+setexpr.s bootargs *0x12000000
 
-fatload mmc 2:1 0x44000000 exynos5422-odroidhc1.dtb
+fatload mmc 2:1 0x1000000 meson-gxbb-odroidc2.dtb
 
-fdt addr 0x44000000
+fdt addr 0x1000000
 
-bootz 0x40008000 - 0x44000000
+bootz 0x10008000 - 0x14000000
